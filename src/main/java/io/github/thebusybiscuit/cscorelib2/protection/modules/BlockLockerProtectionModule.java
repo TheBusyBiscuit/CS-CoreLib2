91425ed8cdd71b3f3c5a82f60b8080e6da244069
PhanaticD <javw.phanatic@gmail.com>
2019-11-19 09:43:20 -0500

    Use blocklockers API to check permission

--- src/main/java/io/github/thebusybiscuit/cscorelib2/protection/modules/BlockLockerProtectionModule.java
+++ src/main/java/io/github/thebusybiscuit/cscorelib2/protection/modules/BlockLockerProtectionModule.java
@@ -3,14 +3,10 @@ package io.github.thebusybiscuit.cscorelib2.protection.modules;
 import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
 
-import com.google.common.base.Optional;
-
 import io.github.thebusybiscuit.cscorelib2.protection.ProtectableAction;
 import io.github.thebusybiscuit.cscorelib2.protection.ProtectionModule;
 import nl.rutgerkok.blocklocker.BlockLockerAPI;
 import nl.rutgerkok.blocklocker.BlockLockerPlugin;
-import nl.rutgerkok.blocklocker.profile.Profile;
-import nl.rutgerkok.blocklocker.protection.Protection;
 
 
 public class BlockLockerProtectionModule implements ProtectionModule {
@@ -30,17 +26,8 @@ public class BlockLockerProtectionModule implements ProtectionModule {
 	@Override
 	public boolean hasPermission(OfflinePlayer p, Location l, ProtectableAction action) {
 		if (!action.isBlockAction()) return true;
-		
-		Optional<Protection> protection = plugin.getProtectionFinder().findProtection(l.getBlock());
-		
-		if (protection.isPresent()) {
-			Profile profile = plugin.getProfileFactory().fromNameAndUniqueId(p.getName(), Optional.of(p.getUniqueId()));
-			return protection.get().isAllowed(profile);
-		}
-		else {
-			return true;
-		}
-		
+
+		return(BlockLockerAPI.isAllowed(p.getPlayer(), l.getBlock(), false));
 	}
 }
 	
